aca85e9570868151ded510ca91701a116e272121
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2016-08-17 19:54:51 +0000

    added comment generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,12 @@ posts = Post.all
   Post.find_or_create_by(title: "A unique title", body: "A unique body")
   puts "#{Post.count}"
   
+  puts "#{Comment.count}"
+  Comment.find_or_create_by(body: "A unique comment")
+  puts "#{Comment.count}"
+  
   puts "Seed finished"
   puts "#{Post.count} posts created"
   puts "#{Comment.count} comments created"
+  
+  
