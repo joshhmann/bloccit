e464de746c31bc512d6577fd1bfd86ff54e90ca7
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2016-09-12 05:22:12 +0000

    Created Topics page, refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+    resources :topics do
+        
+        resources :posts, except: [:index]
+    end
 
-resources :posts
+    get 'about' => 'welcome#about'
 
-get 'about' => 'welcome#about'
-
-root 'welcome#index'
+    root 'welcome#index'
 
 end
