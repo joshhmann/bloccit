ca4042ab398fe1777c9f52b8d70cb1aeebf792ca
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2016-11-16 09:17:25 +0000

    added users to allow ownership of posts

---
+++ d
@@ -0,0 +1,7 @@
+=> [32m#<User:0x00000004127b50[0m
+ id: [1;34m6[0m,
+ name: [31m[1;31m"[0m[31mBloccer[1;31m"[0m[31m[0m,
+ email: [31m[1;31m"[0m[31muser@bloccit.com[1;31m"[0m[31m[0m,
+ password_digest: [31m[1;31m"[0m[31m$2a$10$IoX/ExVk5lbd.TAMbFpPWexZ6EjSYcrGJZ4Lqz5hYWjEBWF1f.ygO[1;31m"[0m[31m[0m,
+ created_at: [1;34;4mWed[0m, [1;34m16[0m [1;34;4mNov[0m [1;34m2016[0m [1;34m07[0m:[1;34m13[0m:[1;34m46[0m [1;34;4mUTC[0m [1;34m+00[0m:[1;34m00[0m,
+ updated_at: [1;34;4mWed[0m, [1;34m16[0m [1;34;4mNov[0m [1;34m2016[0m [1;34m07[0m:[1;34m13[0m:[1;34m46[0m [1;34;4mUTC[0m [1;34m+00[0m:[1;34m00[0m[32m>[0m
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 require 'random_data'
 
+# Create Users
+
+5.times do 
+  User.create!(
+    
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
+
+
+
+# Create topics
 15.times do 
   Topic.create!(
     name:        RandomData.random_sentence,
@@ -15,6 +30,7 @@ require 'random_data'
     
     # 1 
   Post.create!(
+      user: users.sample,
       topic: topics.sample,
       title: RandomData.random_sentence,
       body: RandomData.random_paragraph
@@ -32,7 +48,14 @@ posts = Post.all
     )
   end
   
+  user = User.first
+  user.update_attributes!(
+    email: 'joshua.frederick.anderson@gmail.com',
+    password: 'helloworld'
+    )
+  
   puts "Seed finished"
+  puts "#{User.count} users created"
   puts "#{Topic.count} topics created"
   puts "#{Post.count} posts created"
   puts "#{Comment.count} comments created"
