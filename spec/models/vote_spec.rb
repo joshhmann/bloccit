c903b82e56b98b69db7fcc8660d408510bddd3ff
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2017-06-02 02:22:57 +0000

    partial vote WIP

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -9,5 +9,5 @@ RSpec.describe Vote, type: :model do
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:value) }
-  it { is_expected.to validate_presence_of(:value).in_array([-1, 1]) }
+  it { is_expected.to validate_inclusion_of(:value).in_array( [-1, 1]) }
 end
