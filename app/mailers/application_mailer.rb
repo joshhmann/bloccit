d161f9b3c7dbcca7514fdcdcfe392aa5ad68f1cb
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2017-07-18 01:32:13 +0000

    Adding a callback

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,5 +3,14 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
   
   def new_comment(user, post, comment)
-    headers
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    
+    @user = user
+    @post = post
+    @comment = comment
+    
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
 end
