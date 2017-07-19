6e5e285d3300be4951602ab6a5acc201392c33f4
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2017-07-19 20:36:24 +0000

    Implementing Gravatars - user_spec.rb just ran specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ include RandomData
 include SessionsHelper
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { create(:user) }
 
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
@@ -27,7 +27,7 @@ RSpec.describe User, type: :model do
 
   describe "attributes" do
     it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+      expect(user).to have_attributes(name: user.name, email: user.email)
     end
   end
   describe "methods" do
@@ -104,6 +104,15 @@ RSpec.describe User, type: :model do
     it "returns the apropriate favorite if it exists" do
       favorite = user.favorites.where(post: @post).create
       expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  end
+  
+  describe ".avatar_url" do
+    let(:known_user) { create(:user, email: "blochead@bloc.io") }
+
+    it "returns the proper Gravatar url for a known email entity" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
     end
   end
 end
