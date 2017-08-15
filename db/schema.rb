521669c69044c8fe8cbd1364dad17a75facc0294
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2017-08-15 01:46:30 +0000

    On fetching API Data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607072239) do
+ActiveRecord::Schema.define(version: 20170814234746) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20170607072239) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
