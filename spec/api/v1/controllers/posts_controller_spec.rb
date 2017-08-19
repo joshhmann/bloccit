aa223a6893dbf8f85722246d28905c1b68474f18
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2017-08-19 06:20:42 +0000

    Finished ass 48

--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Api::V1::PostsController, type: :controller do
   let(:my_user) { create(:user) }
-  let(:my_post) { create(:post) }
+  let(:my_topic) { create(:topic) }
+  let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
   
   context "unauthenticated user" do
     it "GET index returns http success" do
